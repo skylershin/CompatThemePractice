e0114ed0a4258422866213a0326096e3690ad003
MunkyuShin <munkyu.s.shin@gmail.com>
2017-01-20 20:40:14 +0900

    Status bar color customizing

--- app/src/main/java/com/example/compatthemepractice/MainActivity.java
+++ app/src/main/java/com/example/compatthemepractice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.compatthemepractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Main");
+        setSupportActionBar(toolbar);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
     }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+
 }
